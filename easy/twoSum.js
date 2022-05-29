edef132727061acc30c596366881ef566b503d1e
Michael <MReyna12@live.com>
2022-05-29 10:49:24 -0500

    update

--- easy/twoSum.js
+++ easy/twoSum.js
@@ -11,9 +11,23 @@
 const twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
+      console.log(i, j);
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
     }
   }
 };
+
+function twoSum(nums, target) {
+  const object = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    if (object[target - number] !== undefined) {
+      return [object[target - number], i];
+    }
+    object[number] = i;
+  }
+  return [];
+}
