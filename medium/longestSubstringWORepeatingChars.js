782b563975bd384084ed57b3ecb520a8109fe35d
Michael <MReyna12@live.com>
2022-05-30 11:56:56 -0500

    medium leetcode

---
+++ medium/longestSubstringWORepeatingChars.js
@@ -0,0 +1,13 @@
+function lengthOfLongestSubstring(s) {
+  const map = {};
+  let left = 0;
+
+  return s.split("").reduce((max, char, index) => {
+    const lastSeenIndex = map[char];
+    const windowContainsCharacter = lastSeenIndex >= left;
+    left = windowContainsCharacter ? lastSeenIndex + 1 : left;
+    map[char] = index;
+    const windowSize = index - left + 1;
+    return Math.max(max, windowSize);
+  }, 0);
+}
