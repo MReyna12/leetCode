515e2e9e0599a66934ed3e0a027f3afb260952e8
Michael <MReyna12@live.com>
2022-07-20 15:19:18 -0500

    linked list practice

--- easy/structy-ll-practice.js
+++ easy/structy-ll-practice.js
@@ -315,14 +315,43 @@ const recursiveReverseList = (head, prev = null) => {
 // Zipper lists
 
 // Iteration
+// const zipperLists = (head1, head2) => {
+//   let tail = head1;
+//   let current1 = head1.next;
+//   let current2 = head2;
+//   let count = 0;
+
+//   while (current1 !== null && current2 !== null) {
+//     if (count % 2 === 0) {
+//       tail.next = current2;
+//       current2 = current2.next;
+//     } else {
+//       tail.next = current1;
+//       current1 = current1.next;
+//     }
+//     tail = tail.next;
+//     count += 1;
+//   }
+
+//   if (current1 !== null) {
+//     tail.next = current1;
+//   }
+//   if (current2 !== null) {
+//     tail.next = current2;
+//   }
+
+//   return head1;
+// };
+
+// Practice Attempt - 1
 const zipperLists = (head1, head2) => {
   let tail = head1;
   let current1 = head1.next;
   let current2 = head2;
-  let count = 0;
+  let counter = 0;
 
   while (current1 !== null && current2 !== null) {
-    if (count % 2 === 0) {
+    if (counter % 2 === 0) {
       tail.next = current2;
       current2 = current2.next;
     } else {
@@ -330,22 +359,33 @@ const zipperLists = (head1, head2) => {
       current1 = current1.next;
     }
     tail = tail.next;
-    count += 1;
-  }
+    counter += 1;
 
-  if (current1 !== null) {
-    tail.next = current1;
-  }
-  if (current2 !== null) {
-    tail.next = current2;
+    if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
   }
 
   return head1;
 };
 
-// Practice Attempt - 1
+//console.log(zipperLists(a, two));
 
 // Recursion
+// const recursiveZipperList = (head1, head2) => {
+//   if (head1 === null && head2 === null) return null;
+//   if (head1 === null) return head2;
+//   if (head2 === null) return head1;
+
+//   const next1 = head1.next;
+//   const next2 = head2.next;
+//   head1.next = head2;
+
+//   head2.next = recursiveZipperList(next1, next2);
+
+//   return head1;
+// };
+
+// Practice Attempt - 1
 const recursiveZipperList = (head1, head2) => {
   if (head1 === null && head2 === null) return null;
   if (head1 === null) return head2;
@@ -356,11 +396,7 @@ const recursiveZipperList = (head1, head2) => {
   head1.next = head2;
 
   head2.next = recursiveZipperList(next1, next2);
-
   return head1;
 };
 
-// Practice Attempt - 1
-
-//console.log(zipperLists(a, two));
 //console.log(recursiveZipperList(a, two));
