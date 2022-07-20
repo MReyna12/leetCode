2bb2ee598d4dfb98fcefe6d7689abfd50cfb70c0
Michael <MReyna12@live.com>
2022-07-20 16:53:37 -0500

    linked list practice

--- easy/merge-two-sorted-lists.js
+++ easy/merge-two-sorted-lists.js
@@ -12,11 +12,13 @@ const tres = new ListNode(3, quatro);
 const uno = new ListNode(1, tres);
 
 const mergeTwoLists = (list1, list2) => {
-  let newList = new ListNode();
-  let tail = newList;
+  // Create new list for nodes to be merged into
+  let list3 = new ListNode();
+  let tail = list3;
 
+  // While list1 and list2 are not empty
   while (list1 !== null && list2 !== null) {
-    if (list1.val < list2.val) {
+    if (list1.val <= list2.val) {
       tail.next = list1;
       list1 = list1.next;
     } else {
@@ -29,7 +31,7 @@ const mergeTwoLists = (list1, list2) => {
   if (list1 !== null) tail.next = list1;
   if (list2 !== null) tail.next = list2;
 
-  return newList.next;
+  return list3.next;
 };
 
 console.log(mergeTwoLists(one, uno));
