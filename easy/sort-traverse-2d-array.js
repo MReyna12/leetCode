d3d4000b4ea0e66e13b5d91cd63369d477b47d46
Michael <MReyna12@live.com>
2022-07-22 10:28:48 -0500

    2d array practice

--- easy/sort-traverse-2d-array.js
+++ easy/sort-traverse-2d-array.js
@@ -60,7 +60,24 @@ function makeMatrix(m, n, value) {
   return array;
 }
 
-//makeMatrix(2, 2, 0);
+function makeMatrix2(m, n, value) {
+  // Create the array
+  let array = [];
+  // Create the rows
+  for (let i = 0; i < m; i++) {
+    let subarray = [];
+    // Create the elements that will fill the subarrays
+    for (let j = 0; j < n; j++) {
+      let elements = value;
+      subarray.push(elements);
+    }
+    array.push(subarray);
+  }
+
+  return array;
+}
+
+//console.log(makeMatrix2(5, 3, "x"));
 
 // Write a function `totalProduct(array)` that accepts a 2D array of numbers. The function should return
 // the total product of all numbers in the array.
@@ -78,11 +95,26 @@ function totalProduct(array) {
   return sum;
 }
 
-// totalProduct([
-//   [4, 6],
-//   [2, 3],
-//   [1, 2],
-// ]);
+function totalProduct2(array) {
+  // Create a sum variable that starts at one so it can be multiplied by the first number in the first subarray
+  let totalProduct = 1;
+  // Create a for loop to access each subarray
+  for (let i = 0; i < array.length; i++) {
+    let subarray = array[i];
+    for (let j = 0; j < subarray.length; j++) {
+      totalProduct *= subarray[j];
+    }
+  }
+  return totalProduct;
+}
+
+// console.log(
+//   totalProduct2([
+//     [4, 6],
+//     [2, 3],
+//     [1, 2],
+//   ])
+// );
 
 // Write a function `twoSumPairs(numbers, target)` that accepts an array of numbers and a target number
 // as arguments. The function should return a 2D array containing all unique pairs of elements that
@@ -104,7 +136,26 @@ function twoSumPairs(numbers, target) {
   return arrayOfPairs;
 }
 
-//console.log(twoSumPairs([10, 7, 4, 5, 2], 12));
+function twoSumPairs2(numbers, target) {
+  // Create empty array that will hold the subarrays containing unique pairs of elements that sum to the target
+  let array = [];
+
+  // Create first loop to hold an element value and check if adding next element equals target, if not then skip to next element
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        let element1 = numbers[i];
+        let element2 = numbers[j];
+        let pair = [element1, element2];
+        array.push(pair);
+      }
+    }
+  }
+
+  return array;
+}
+
+//console.log(twoSumPairs2([10, 7, 4, 5, 2], 12));
 
 // Write a function `zipper` that accepts two arrays as arguments. The function should return a 2D
 // array containing pairs of elements at the same indices. You can assume that the arrays have the
@@ -129,7 +180,20 @@ function zipper(array1, array2) {
   return arrayOfPairs;
 }
 
-//console.log(zipper(["whisper", "talk", "shout"], ["quiet", "normal", "loud"]));
+function zipper2(array1, array2) {
+  let array = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    let element1 = array1[i];
+    let element2 = array2[i];
+    let pair = [element1, element2];
+    array.push(pair);
+  }
+
+  return array;
+}
+
+//console.log(zipper2(["whisper", "talk", "shout"], ["quiet", "normal", "loud"]));
 
 // Bubble Sort Practice
 function bubbleSort(array) {
@@ -145,9 +209,26 @@ function bubbleSort(array) {
   console.log(array);
 }
 
-//const arr = [234, 43, 55, 63, 5, 6, 235, 547];
+function bubbleSort2(array) {
+  let arrayCopy = array.slice();
 
-//bubbleSort(arr);
+  // First for loop to determine how many times we need to traverse the array
+  for (let i = 0; i < arrayCopy.length - 1; i++) {
+    // Second for loop to compare current and next elements and move current forward if greater than next element; comparison is less each time because we will lock the greater values in place
+    for (let j = 0; j < arrayCopy.length - i - 1; j++) {
+      if (arrayCopy[j] > arrayCopy[j + 1]) {
+        // Swap places of the two elements
+        [arrayCopy[j], arrayCopy[j + 1]] = [arrayCopy[j + 1], arrayCopy[j]];
+      }
+    }
+  }
+
+  return arrayCopy;
+}
+
+const arr = [234, 43, 55, 63, 5, 6, 235, 547];
+
+//console.log(bubbleSort2(arr));
 
 function bubble(array) {
   // Create a shallow copy of the input array
