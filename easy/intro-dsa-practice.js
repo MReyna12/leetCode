57b15314196c6a63a664672db48f3507c7edb72b
Michael <MReyna12@live.com>
2022-07-26 09:39:01 -0500

    intro dsa practice

--- easy/intro-dsa-practice.js
+++ easy/intro-dsa-practice.js
@@ -1,7 +1,19 @@
-const array = [1, 2, 3];
+let array = [1, 2, 3];
 
 const firstEl = array[0];
 
 for (const num of array) {
+  console.log(num);
   if (num === 1) console.log("It is here!");
+  break;
 }
+
+let numbers = [];
+numbers.push(2);
+numbers.push(200);
+
+console.log(numbers);
+
+const newArray = array.concat(numbers);
+
+console.log(newArray);
