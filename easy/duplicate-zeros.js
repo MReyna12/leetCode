0a2cfb5dc9e3a7500d7ae27cda0e2c29904d1b04
Michael <MReyna12@live.com>
2022-07-27 11:25:42 -0500

    easy leetcode

--- easy/duplicate-zeros.js
+++ easy/duplicate-zeros.js
@@ -8,7 +8,7 @@ for (let i = 3; i < intArray.length; i++) {
   intArray[i] = i;
 }
 
-console.log(intArray);
+//console.log(intArray);
 
 for (let i = intArray.length; i >= 0; i--) {
   intArray[i] = intArray[i - 1];
@@ -16,7 +16,7 @@ for (let i = intArray.length; i >= 0; i--) {
     intArray[i] = 10;
   }
 }
-console.log(intArray);
+//console.log(intArray);
 
 for (let i = intArray.length; i > 1; i--) {
   intArray[i] = intArray[i - 1];
@@ -25,4 +25,20 @@ for (let i = intArray.length; i > 1; i--) {
   }
 }
 
-console.log(intArray);
+//console.log(intArray);
+
+// P: Fixed-length array of integers
+// R: No return value
+// E:
+// P: For loop that starts at index 0; if a value in an index is === 0, then shift all numbers after the value over one and make the current zero value equal to zero as well; then skip over the zero that was just added
+
+const duplicateZeros = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      i++;
+    }
+  }
+};
