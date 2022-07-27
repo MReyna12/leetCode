23a537d4f2dcb790ad8bf01ca0d48bc15cf4aa44
Michael <MReyna12@live.com>
2022-07-27 09:45:34 -0500

    easy leetcode

--- easy/find-nums-even-number-digits.js
+++ easy/find-nums-even-number-digits.js
@@ -1,7 +1,7 @@
 // P: Parameter is an array of integers
 // R: Return the total number of integers in the array that contain an even number of digits
 // E: [0, 1, 2, 12] - 1 (12); [0] - 0; [23, 42, 4444] - 3
-// P: Use a for loop to traverse each element of the array; check if the length of the element is divisible by 0 (even) or 1 (odd); if even add 1 to the counter; return counter
+// P: Convert each integer in the array to a string; determine the length of each string, with any being mod 2 === 0 added to a counter; return the counter
 
 function findNumbers(nums) {
   let counter = 0;
@@ -15,3 +15,16 @@ function findNumbers(nums) {
 
   return counter;
 }
+
+const findNumbers = (nums) => {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    const length = `${nums[i]}`.length;
+    if (length % 2 === 0) {
+      count += 1;
+    }
+  }
+
+  return count;
+};
