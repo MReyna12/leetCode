5cd869dda0ceae46ee7c52fce0d88bf31df117a6
Michael <MReyna12@live.com>
2022-07-26 19:24:26 -0500

    easy leetcode

--- easy/squares-sorted-array.js
+++ easy/squares-sorted-array.js
@@ -3,7 +3,7 @@
 // E: [1, 2] - [1, 4]
 // P: Let's start by squaring each number within the array; merge sort the newly square array
 
-const sortedSquares = (nums) => {
+/*const sortedSquares = (nums) => {
   // Create an array to hold the squared values
   let squaredArray = [];
 
@@ -14,7 +14,7 @@ const sortedSquares = (nums) => {
   }
 
   return sortMerge(squaredArray);
-};
+};*/
 
 function sortMerge(array) {
   console.log(array);
@@ -58,5 +58,34 @@ function sortMerge(array) {
   console.log(sortedArray);
   return sortedArray;
 }
+
+//sortedSquares([-4, -1, 0, 3, 10, 20]);
+
+// Two pointers
+
+const sortedSquares = (nums) => {
+  // Create new array with a length the size of the array to be squared & sorted
+  const sorted = new Array(nums.length);
+
+  // Create two pointers, one beginning at the start and the other beginning at the end
+  let head = 0;
+  let tail = sorted.length - 1;
+
+  // Loop through the elements, comparing the squared values of the start and end and place the greater value in the last index. Once value added the index is decreased by one and the process is ran again
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const squaredHead = Math.pow(nums[head], 2);
+    const squaredTail = Math.pow(nums[tail], 2);
+
+    if (squaredHead > squaredTail) {
+      sorted[i] = squaredHead;
+      head++;
+    } else {
+      sorted[i] = squaredTail;
+      tail--;
+    }
+  }
+
+  return sorted;
+};
 
 sortedSquares([-4, -1, 0, 3, 10, 20]);
