e5e07b5807a50d978d0707de553febd4df4ca4fb
Michael <MReyna12@live.com>
2022-07-28 13:56:54 -0500

    easy leetcode practice

---
+++ easy/removed-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+// P: Integer of nums sorted in non-decreasing order
+// R: Return the number of unique elements
+// E:
+// P:
+
+const removeDuplicates = (nums) => {
+  let left = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[left] !== nums[i]) {
+      nums[left + 1] = nums[i];
+      left++;
+    }
+  }
+
+  return left + 1;
+};
+
+removeDuplicates([1, 1, 2]);
