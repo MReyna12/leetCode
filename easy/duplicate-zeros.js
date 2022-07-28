c39667028751faf4a69ddaaf839545793faac6fd
Michael <MReyna12@live.com>
2022-07-28 10:52:07 -0500

    easy leetcode practice

--- easy/duplicate-zeros.js
+++ easy/duplicate-zeros.js
@@ -42,3 +42,15 @@ const duplicateZeros = (arr) => {
     }
   }
 };
+
+// Practice Attempt - 1
+const duplicateZeros1 = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      i++;
+    }
+  }
+};
