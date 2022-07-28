f602773c5aed8e38691ac5eb5e6aaf3124f35813
Michael <MReyna12@live.com>
2022-07-28 11:20:21 -0500

    easy leetcode practice

--- easy/merge-two-sorted-lists.js
+++ easy/merge-two-sorted-lists.js
@@ -35,3 +35,27 @@ const mergeTwoLists = (list1, list2) => {
 };
 
 console.log(mergeTwoLists(one, uno));
+
+// Practice Attempt - 1
+// Merge the two lists, with lesser values going first
+const mergeTwoLists1 = (list1, list2) => {
+  let list3 = new ListNode();
+  let tail = list3;
+
+  // While list1 and list2 are not empty
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      tail.next = list1.val;
+      list1 = list1.next;
+    } else {
+      tail.next = list2.val;
+      list2 = list2.next;
+    }
+    tail = tail.next;
+  }
+
+  if (list1 !== null) tail.next = list1;
+  if (list2 !== null) tail.next = list2;
+
+  return list3.next;
+};
