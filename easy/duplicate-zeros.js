b14bef12c535847d4cd3ac2eb23781559ea8a86a
Michael <MReyna12@live.com>
2022-07-29 09:45:09 -0500

    easy leetcode

--- easy/duplicate-zeros.js
+++ easy/duplicate-zeros.js
@@ -73,3 +73,23 @@ const duplicateZeros2 = (arr) => {
     }
   }
 };
+
+// P: An array of integers
+// R: No explicit return. The outpout should be a modified version of the input array that duplicates zeros within the input array, if any. The length of the input array should not be expanded, so as values are shifted to the right they will be removed.
+// E: [0, 1, 2, 3, 0] - [0, 0, 1, 2, 3]
+// P: Use two pointers - the first pointer begins at index 0 and the last pointer begins at the last element of the input array and decreases after each loop; the first loop checks to see if an element is equal to 0, if so then the second loop runs
+// and shifts each over one to the right, which will duplicate any zeros because the second loop only runs until it is not greater than i (so as to not shift the value over at index i which will be zero because that's the only way the second loop runs)
+
+const duplicateZeros3 = (arr) => {
+  // Create first for loop and a conditional to check the array for elements equal to zero
+  for (let i = 0; i < arr.length; i++) {
+    // Conditional to determine if the second for loop needs to run to shift each element over one to the right
+    if (arr[i] === 0) {
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      // Since we shifted values, we need to increment i one more time or else when it increments by 1 the next value will always be zero
+      i++;
+    }
+  }
+};
