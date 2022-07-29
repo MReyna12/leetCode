72cbf5b10ecba02cfd2d78ab1b4f428a41baf2f9
Michael <MReyna12@live.com>
2022-07-29 13:43:11 -0500

    easy leetcode

---
+++ easy/valid-mountain-array.js
@@ -0,0 +1,25 @@
+// P:
+// R:
+// E:
+// P:
+
+const validMountainArray = (arr) => {
+  if (arr.length < 3) return false;
+
+  let isIncreasing = false;
+  let isDecreasing = false;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] === arr[i + 1]) {
+      return false;
+    } else if (arr[i] < arr[i + 1]) {
+      isIncreasing = true;
+      if (isDecreasing) return false;
+    } else {
+      isDecreasing = true;
+      if (!isIncreasing) return false;
+    }
+  }
+
+  return isDecreasing && isIncreasing;
+};
