fb8c6660651b5fc340229eb1b1984e7b61e380d2
Michael <MReyna12@live.com>
2022-07-29 10:11:57 -0500

    easy leetcode

--- easy/merge-two-sorted-lists.js
+++ easy/merge-two-sorted-lists.js
@@ -59,3 +59,36 @@ const mergeTwoLists1 = (list1, list2) => {
 
   return list3.next;
 };
+
+// Practice Attempt - 2
+
+// P: Two heads for two sorted lists
+// R: Return the head of the merged linked list--said list must remain sorted.
+// E: 1 -> 2 -> 4 & 1 -> 1 -> 3 -> 4 // 1 -> 1 -> 2 -> 3 -> 4 -> 4
+// P: Create a third list with a dummy node; conditional to compare the initial head values, if one is lesser than the other then make it the next node of the dummy node; the list that contained said lesser node value is then shifted to the next node
+// and the comparison begins again; this process continues until one of the two lists are undefined (because we've iterated through all nodes of the applicable list); we then add remaining nodes from list that has not been fully traversed
+// to the new list; we then return the new list, starting at the next node of the initial node in said list because we don't need to include the dummy node
+
+const mergeTwoLists2 = (list1, list2) => {
+  // Third list and dummy node creation
+  const list3 = new ListNode();
+  let dummyNode = list3;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      dummyNode.next = list1;
+      list1 = list1.next;
+    } else {
+      dummyNode.next = list2;
+      list2 = list2.next;
+    }
+    dummyNode = dummyNode.next;
+  }
+
+  // if list1 values still remain then add remainder to new list
+  if (list1 !== null) dummyNode.next = list1;
+  if (list2 !== null) dummyNode.next = list2;
+
+  // Return head of new list
+  return list3.next;
+};
